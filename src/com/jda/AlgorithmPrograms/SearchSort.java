da7606ab4cdeeb18bb5dbf39e652553d2187d139
Kandula Gurunadh <Kandula.Gurunadh@jda.com>
2018-07-10 11:12:45 +0530

    localupload

--- src/com/jda/AlgorithmPrograms/SearchSort.java
+++ src/com/jda/AlgorithmPrograms/SearchSort.java
@@ -1,5 +1,11 @@
+/*  Purpose:	Printing prime numbers below the given number
+ *
+ *  @author  gurunadh
+ *  @version 1.0
+ *  @since   10-07-2017
+ *
+ ******************************************************************************/
 package com.jda.AlgorithmPrograms;
-
 public class SearchSort {
 
 }
