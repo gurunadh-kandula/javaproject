77676ea3e1c421b19c043373d913591447d980c7
Kandula Gurunadh <Kandula.Gurunadh@jda.com>
2018-07-18 14:15:53 +0530

    new upload

---
+++ src/com/jda/utility/Queue.java
@@ -0,0 +1,43 @@
+package com.jda.utility;
+
+public class Queue<T extends Comparable<T>> {
+	public Qnode<T> front = null;
+	public Qnode<T> rear = null;
+
+	public static class Qnode<S> {
+		public S value;
+		public Qnode<S> next;
+        public String purpose=null;
+		Qnode(S data,String pur) {
+			value = data;
+			purpose=pur;
+			next = null;
+		}
+	}
+
+	public Queue() {
+		rear = front = null;
+	}
+
+	public void enqueue(T data,String purpose) {
+		Qnode<T> newnode = new Qnode<T>(data,purpose);
+		if (rear == null) {
+			rear = front = newnode;
+			return;
+		}
+		rear.next = newnode;
+		rear = newnode;
+	}
+
+	public Qnode<T> dequeue() {
+		if (front == null)
+			return null;
+		Qnode<T> temp=front;
+		front = front.next;
+		if (front == null)
+			rear = null;
+		return temp;
+
+	}
+	
+}
